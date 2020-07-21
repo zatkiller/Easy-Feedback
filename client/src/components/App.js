483781da683e744ee9c0bf104da04cf482ae3b12
zatkiller <gheedylan@yahoo.com.sg>
2020-07-22 02:54:36 +0800

    Added header component and landing page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+
+const Landing = () => <h2>Landing</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
 
 const App = () => {
-	return <div>Hi you!</div>;
+	return (
+		<div className="container">
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/surveys" component={Dashboard} />
+					<Route path="/surveys/new" component={SurveyNew} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import "materialize-css/dist/css/materialize.min.css"; // not ./, JS will assume import is from node modules
 
 import App from "./components/App";
+import reducers from "./reducers";
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
 	<Provider store={store}>
