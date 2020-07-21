ce2d96123b024e181d6a815aefe9bc0a5319e3d6
zatkiller <gheedylan@yahoo.com.sg>
2020-07-21 22:32:30 +0800

    Final commit before adding client side

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
+const cors = require("cors");
+
 require("./models/User");
 require("./services/passport"); //passport Configs
 
@@ -10,6 +12,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(cors());
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000, //milli seconds
