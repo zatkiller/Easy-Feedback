b75216fac4ec58da0aa69d21522fcdb10b1196af
zatkiller <gheedylan@yahoo.com.sg>
2020-07-23 05:49:54 +0800

    Added login and logout routes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,39 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 class Header extends Component {
+	//Helper function
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return <li><a href="/auth/google">Login With Google</a></li>;
+			default:
+				return <li><a href="/api/logout">Logout</a></li>
+		}
+	}
+
 	render() {
 		return (
 			<nav>
 				<div className="nav-wrapper">
 					<a className="left brand-logo">Emaily</a>
 					<ul className="right">
-						<li>
-							<a>Login with Google</a>
-						</li>
+						{this.renderContent()}
 					</ul>
 				</div>
 			</nav>
 		);
 	}
 }
-export default Header;
+
+// function mapStateToProps(state) {
+// 	return { auth: state.auth };
+// }
+
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
