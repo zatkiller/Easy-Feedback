b416b335f68771347099345d4205a2542d6f62b3
zatkiller <gheedylan@yahoo.com.sg>
2020-07-29 05:16:25 +0800

    SendGrid tested and working

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,62 +7,59 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-
-	app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
-		const { title, subject, body, recipients } = req.body; //req from front end
+	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+		const { title, subject, body, recipients } = req.body;
 
 		const survey = new Survey({
-			title, //ES6 syntax
+			title,
 			subject,
 			body,
-			recipients: recipients
-				.split(",")
-				.map((email) => ({ email: email.trim() })), //trim to remove whitespace
+			recipients: recipients.split(',').map(email => ({ email: email.trim() })),
 			_user: req.user.id,
-			dateSent: Date.now(),
+			dateSent: Date.now()
 		});
 
+		const mailer = new Mailer(survey, surveyTemplate(survey));
 		try {
-			Mailer(survey, surveyTemplate(survey));
-			await survey.save(); // mongoose
+			await mailer.send();
+			await survey.save();
 			req.user.credits -= 1;
 			const user = await req.user.save();
 
-			res.send(user); // Update via mongoose
-		}
-		catch (err) {
+			res.send(user);
+		} catch (err) {
 			res.status(422).send(err);
 		}
 	});
 
-	app.get('/api/surveys/thanks', (req, res) => {
-		res.send("Thanks");
-	});
-
-	//Old API
-	// app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-	// 	const { title, subject, body, recipients } = req.body;
+	// app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
+	// 	const { title, subject, body, recipients } = req.body; //req from front end
 
 	// 	const survey = new Survey({
-	// 		title,
+	// 		title, //ES6 syntax
 	// 		subject,
 	// 		body,
-	// 		recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+	// 		recipients: recipients
+	// 			.split(",")
+	// 			.map((email) => ({ email: email.trim() })), //trim to remove whitespace
 	// 		_user: req.user.id,
-	// 		dateSent: Date.now()
+	// 		dateSent: Date.now(),
 	// 	});
 
-	// 	const mailer = new Mailer(survey, surveyTemplate(survey));
-
 	// 	try {
-	// 		await mailer.send();
-	// 		await survey.save();
+	// 		Mailer(survey, surveyTemplate(survey));
+	// 		await survey.save(); // mongoose
 	// 		req.user.credits -= 1;
 	// 		const user = await req.user.save();
 
-	// 		res.send(user);
-	// 	} catch (err) {
+	// 		res.send(user); // Update via mongoose
+	// 	}
+	// 	catch (err) {
 	// 		res.status(422).send(err);
 	// 	}
+	// });
+
+	// app.get('/api/surveys/thanks', (req, res) => {
+	// 	res.send("Thanks");
 	// });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -1,82 +1,80 @@
-const sgMail = require('@sendgrid/mail'); //Pode package
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-module.exports = async ({ subject, recipients }, content) => {
-    // using SendGrid's v3 Node.js Library
-    // https://github.com/sendgrid/sendgrid-nodejs
-    sgMail.setApiKey(keys.sendGridKey);
-    const formattedRecipients = recipients.map(({ email }) => email);
-    const msg = {
-        to: formattedRecipients,
-        from: "gheedylan@yahoo.com.sg",
-        subject: subject,
-        html: content,
-    };
-    try {
-        await sgMail.send(msg);
-    } catch (error) {
-        console.error(error);
+class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
 
-        if (error.response) {
-            console.error(error.response.body)
-        }
+        this.sgApi = sendgrid(keys.sendGridKey);
+        this.from_email = new helper.Email('zatkiller123@gmail.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+        this.addContent(this.body); //add to the Mailer, add Content is a built in helper function from helper.Mail
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        });
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+
+        const response = await this.sgApi.API(request).catch(err => console.log(err))
+        return response;
     }
 }
+module.exports = Mailer;
 
-//Old API
-// const sendgrid = require('sendgrid');
-// const helper = sendgrid.mail;
+// const sgMail = require('@sendgrid/mail'); //Pode package
 // const keys = require('../config/keys');
 
-// class Mailer extends helper.Mail {
-//     constructor({ subject, recipients }, content) {
-//         super();
+// module.exports = async ({ subject, recipients }, content) => {
+//     // using SendGrid's v3 Node.js Library
+//     // https://github.com/sendgrid/sendgrid-nodejs
+//     sgMail.setApiKey(keys.sendGridKey);
+//     const formattedRecipients = recipients.map(({ email }) => email);
+//     const msg = {
+//         to: formattedRecipients,
+//         from: 'zatkiller123@gmail.com',
+//         subject: subject,
+//         html: content,
+//     };
+//     try {
+//         await sgMail.send(msg);
+//     } catch (error) {
+//         console.error(error);
 
-//         this.sgApi = sendgrid(keys.sendGridKey);
-//         this.from_email = new helper.Email('no-reply@emaily.com');
-//         this.subject = subject;
-//         this.body = new helper.Content('text/html', content);
-//         this.recipients = this.formatAddresses(recipients);
-
-//         this.addContent(this.body); //add to the Mailer, add Content is a built in helper function from helper.Mail
-//         this.addClickTracking();
-//         this.addRecipients();
-//     }
-
-//     formatAddresses(recipients) {
-//         return recipients.map(({ email }) => {
-//             return new helper.Email(email);
-//         });
-//     }
-
-//     addClickTracking() {
-//         const trackingSettings = new helper.TrackingSettings();
-//         const clickTracking = new helper.ClickTracking(true, true);
-
-//         trackingSettings.setClickTracking(clickTracking);
-//         this.addTrackingSettings(trackingSettings);
-//     }
-
-//     addRecipients() {
-//         const personalize = new helper.Personalization();
-
-//         this.recipients.forEach(recipient => {
-//             personalize.addTo(recipient);
-//         });
-
-//         this.addPersonalization(personalize);
-//     }
-
-//     async send() {
-//         console.log("Sent")
-//         const request = this.sgApi.emptyRequest({
-//             method: 'POST',
-//             path: '/v3/mail/send',
-//             body: this.toJSON()
-//         });
-
-//         const response = await this.sgApi.API(request).catch(err => console.log(err))
-//         return response;
+//         if (error.response) {
+//             console.error(error.response.body)
+//         }
 //     }
 // }
-// module.exports = Mailer;
